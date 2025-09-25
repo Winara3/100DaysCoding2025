956cfb907030bc052fa403d7ffac8ce7fa57d721
Winara3 <winarapurnama@gmail.com>
2025-09-25 22:22:47 +0800

    tipe data kecil ke tipe data besar

--- Day018/main.java
+++ Day018/main.java
@@ -2,15 +2,10 @@ package Day018;
 
 public class main {
     public static void main(String[] args) {
-       //Int
-        int AngkaInt = 10;
+        int angka  = 1000000;
+        long angkaBesar = angka;
 
-        //Double
-        double AngkaDouble = AngkaInt;
-
-        //otomatis dikonversi ke dauble
-        System.out.println("Nilai Int: " + AngkaInt);
-        System.out.println("Nilai Double: " + AngkaDouble); 
+        System.out.println(angkaBesar);
     }
     
 }
