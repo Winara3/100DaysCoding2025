b69a28dc8263a2eee7dbfd4b017084b9b726d8f1
Winara3 <winarapurnama@gmail.com>
2025-09-25 20:09:07 +0800

    tipe data kecil ketipe data besar

--- Day016/main.java
+++ Day016/main.java
@@ -1,23 +1,22 @@
 package Day016;
 
-public class main {
-    public static void main(String[] args) {
-        int a = 10;
-        int b  = 3;
+public class Main {
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 3;
 
-        //Perkalian
-        int hasilkali = a*b;
-        System.out.println("Hasil perkalian: " + hasilkali);
+    // Perkalian
+    int hasilkali = a * b;
+    System.out.println("Hasil perkalian: " + hasilkali);
 
-        //pembagian Int
-        int hasilbagi1 = a/b;
-        System.out.println("Hasil Bagi (Int): " + hasilbagi1);
-        
-        //Pembagian Double
-        double hasilbagi2 = a/b;
-        System.out.println("Hasil Bagi (Double): " + hasilbagi2);
+    // pembagian Int
+    int hasilbagi1 = a / b;
+    System.out.println("Hasil Bagi (Int): " + hasilbagi1);
 
+    // Pembagian Double
+    double hasilbagi2 = a / b;
+    System.out.println("Hasil Bagi (Double): " + hasilbagi2);
 
-    }
-    
+  }
+
 }
---
+++ Day018/main.java
@@ -0,0 +1,16 @@
+package Day018;
+
+public class main {
+    public static void main(String[] args) {
+       //Int
+        int AngkaInt = 10;
+
+        //Double
+        double AngkaDouble = AngkaInt;
+
+        //otomatis dikonversi ke dauble
+        System.out.println("Nilai Int: " + AngkaInt);
+        System.out.println("Nilai Double: " + AngkaDouble); 
+    }
+    
+}
