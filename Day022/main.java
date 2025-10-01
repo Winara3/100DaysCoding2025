07185bdd3f77008a293d6ada210a0d33b393c954
Winara3 <winarapurnama@gmail.com>
2025-10-01 20:47:24 +0800

    Latihan program luas lingkaran

---
+++ Day022/main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class main   {
+        public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in); 
+
+        System.out.print("Masukkan panjang sisi: ");
+        double sisi = in.nextDouble();
+        
+        System.out.println(sisi*sisi);
+    } 
+    
+}
