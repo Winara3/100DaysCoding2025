d8f5a86f0aaa07c1eee23eefb510885ee899d205
Winara3 <winarapurnama@gmail.com>
2025-10-10 23:17:24 +0800

    NOT (!)

--- Evaluasi1/main.java
+++ Evaluasi1/main.java
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-package Evaluasi1;
-
-public class main {
-    public static void main(String[] args) {
-        String a = "Hava";
-        String b = "Basket";
-        
-        System.out.println("Halo, nama saya " + a);
-        System.out.println("Hobi saya bemain " + b);
-        
-    }
-    
-}
-=======
 package Evaluasi1;
 
 public class main {
@@ -24,4 +9,3 @@ public class main {
         System.out.println("Hobi saya bemain " + b);
     }
 }
->>>>>>> 31acab1af8cf0acc8066a6cee525bcf87da2754d
